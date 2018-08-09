88cdaeccbf2373e5a4a77de1df8e8ed4ec33f45b
unknown <msvendsentan@gmail.com>
2018-08-08 21:16:00 -0400

    update

--- W12D1/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
+++ W12D1/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
@@ -25,8 +25,13 @@ CREATE TABLE favorite_songs (
 
 CREATE TABLE favorite_movies (
   -- Create a numeric column called "id" which automatically increments and cannot be null --
+  id INTEGER NOT NULL AUTO_INCREMENT,
   -- Create a string column called "movie" which cannot be null --
+  movie VARCHAR(30) NOT NULL,
   -- Create a boolean column called "five_times" that sets the default value to false if nothing is entered --
+  five_times BOOLEAN DEFAULT false,
   -- Make an integer column called "score" --
+  score INTEGER(30),
   -- Set the primary key of the table to id --
+  PRIMARY KEY (id)
 );
